a929a137f4ce98c40edcb8fa33875c21beee62ef
singhg32 <113180869+singhg32@users.noreply.github.com>
2023-03-09 13:55:00 +0000

    Add project files.

---
+++ Lessons/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+class Solution
+{
+    public int BinaryCount(int n)
+    {
+
+        int greatestcount = 0;
+        int counter = 0;
+        string check = Convert.ToString(n, 2);
+        foreach (char c in check)
+        {
+            if (c == '1')
+            {
+                if (counter > greatestcount)
+                {
+                    greatestcount = counter;
+                }
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+            
+ 
+            
+        }
+
+        return greatestcount;
+    }
+
+
+
+
+    public int[] CyclicalRotation(int[] A, int K)
+    {
+        int[] B = new int[A.Length];
+        for (int i = 0; i < K; i++)
+        {
+            for (int j = 0; j < A.Length; j++)
+            {
+                if (j == 0)
+                {
+                    B[j] = A[A.Length - 1];
+                }
+                else
+                {
+                    B[j] = A[j - 1];
+                }
+            }
+        }
+        return A;
+    }
+    public int OddOccurences(int[] A)
+    {
+        Dictionary<int, int> pairs = new Dictionary<int, int>();
+        foreach (int a in A)
+        {
+            if (pairs.ContainsKey(a))
+            {
+                pairs[a]++;
+            }
+            else
+            {
+                pairs.Add(a, 1);
+            }
+        }
+        foreach (var x in pairs)
+        {
+            if (x.Value % 2 == 1)
+            {
+                return x.Value;
+            }
+        }
+        return 0;
+    }
+    public int FrogJump(int X, int Y, int D)
+    {
+        int distance = Math.Abs(Y - X);
+        double check = distance / D;
+
+        int Jumps = (int)check;
+        if (distance % D > 0)
+        {
+            Jumps++;
+        }
+        return Jumps;
+    }
+
+    public int PermMissingElem(int[] A)
+    {
+        Array.Sort(A);
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] != (i + 1))
+            {
+                return i + 1;
+            }
+
+        }
+        return 0;
+
+    }
+    static int test()
+    {
+        double L = 3.6444;
+        return (int)L;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(test());
+    }
+
+}
